a6f4e27d07fdd18a76323252f48fb2e4890ae08d
Frank Wiles <frank@revsys.com>
2013-05-22 14:56:47 -0500

    Finish fixing self-friendship/self-following issue

--- friendship/models.py
+++ friendship/models.py
@@ -324,6 +324,9 @@ class FollowingManager(models.Manager):
 
     def add_follower(self, follower, followee):
         """ Create 'follower' follows 'followee' relationship """
+        if follower == followee:
+            raise ValidationError("Users cannot follow themselves")
+
         relation = Follow.objects.create(follower=follower, followee=followee)
 
         follower_created.send(sender=self, follower=follower)
@@ -379,3 +382,9 @@ class Follow(models.Model):
 
     def __unicode__(self):
         return "User #%d follows #%d" % (self.follower_id, self.followee_id)
+
+    def save(self, *args, **kwargs):
+        # Ensure users can't be friends with themselves
+        if self.follower == self.followee:
+            raise ValidationError("Users cannot follow themselves.")
+        super(Follow, self).save(*args, **kwargs)
